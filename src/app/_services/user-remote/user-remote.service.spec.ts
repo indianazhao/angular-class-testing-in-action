25992cc3f839d3e68dd1f37772636d6c3a304a77
indianazhao <indiana.zhao@gmail.com>
2022-06-22 13:31:19 +0800

    SE-3-Part-14 Implementing The JWT logic (完成 UserLlamaService.create 方法，並從 accessToken 取得 payload)

--- src/app/_services/user-remote/user-remote.service.spec.ts
+++ src/app/_services/user-remote/user-remote.service.spec.ts
@@ -3,7 +3,7 @@ import { UserCredentials } from './../../_types/user-credentials.type';
 import { TestBed, fakeAsync } from '@angular/core/testing';
 import { Spy, createSpyFromClass } from 'jasmine-auto-spies';
 import { HttpAdapterService } from '../adapters/http-adapter/http-adapter.service';
-import { UserRemoteService } from './user-remote.service';
+import { UserRemoteService, USER_REMOTE_PATH } from './user-remote.service';
 
 describe('UserRemoteService', () => {
   let serviceUnderTest: UserRemoteService;
@@ -30,39 +30,33 @@ describe('UserRemoteService', () => {
   describe('METHOD: create', () => {
 
     let fakeCredentials: UserCredentials;
-    let expectedUserId: number;
 
-    Given(() => {
-      expectedUserId = undefined;
-    });
+    // 使用 jwt.io 建立一個回來用，不需要去處理 JWT 的問題 (這應該是後端要負責的部份)
+    const accessTokenWithUserIdOf2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsIm5hbWUiOiJGQUtFIFVTRVIiLCJpYXQiOjE1MTYyMzkwMjJ9.R51Wh-iafjESs9CI45tDVlBHEwSaWhwBcZqwH8NVw50';
+    const expectedUserId = 2;
 
-
-    When(fakeAsync(() => {
-      actualResult = serviceUnderTest.create(fakeCredentials);
+    When(fakeAsync(async () => {
+      actualResult = await serviceUnderTest.create(fakeCredentials);
     }));
 
     describe('GIVEN user created successfully THEN return user id', () => {
       Given(() => {
-        // user created successfully
-
-        expectedUserId = 12345;
         fakeCredentials = {
           email: 'fake@fake.com',
           password: 'FAKE PASSWORD',
         };
 
-        const expectedUrl = `/api/user`;
+        const expectedUrl = USER_REMOTE_PATH;
 
         httpAdapterServiceSpy.post
           .mustBeCalledWith(expectedUrl, fakeCredentials)
           .resolveWith({
-            accessToken: 'FAKE TOKEN',
+            accessToken: accessTokenWithUserIdOf2,
           });
 
-      });
+        });
       Then(() => {
-        // return user id
-        expect(actualResult).toHaveBeenCalledWith(expectedUserId);
+        expect(actualResult).toEqual(expectedUserId);
       });
     });
 
--- src/app/_services/user-remote/user-remote.service.ts
+++ src/app/_services/user-remote/user-remote.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
 import { UserCredentials } from 'src/app/_types/user-credentials.type';
+import { HttpAdapterService } from '../adapters/http-adapter/http-adapter.service';
+
+export const USER_REMOTE_PATH = '/api/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRemoteService {
 
-  constructor() { }
+  constructor(
+    private httpAdapterService: HttpAdapterService,
+  ) { }
 
   // TODO: TEST
   // 因為 json-server 使用的 resource id 是 number，所以這邊原本是 token string，改成 user id number
-  create(credentials: UserCredentials): Promise<number> {
-    throw new Error('Method not implemented.');
+  async create(credentials: UserCredentials): Promise<number> {
+    // 必須給 post<T> 加上正確的型別 T，否則後面掉用 tokenHolder.accessToken 會錯誤
+    const tokenHolder = await this.httpAdapterService
+      .post<{ accessToken: string }>(USER_REMOTE_PATH, credentials);
+
+    const userId = this.getUserIdFromToken(tokenHolder.accessToken);
+    return userId;
+  }
+
+  private getUserIdFromToken(token: string): number {
+    // 從 accessToken 取得 payload
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const userId = payload.sub;
+    return userId;
   }
 }
